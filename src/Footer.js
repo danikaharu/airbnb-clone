2e70b1917ad0ed3a8e98c3d6af38144967aee178
danikaharu11 <clashhaki55@gmail.com>
2020-10-09 22:26:03 +0800

    Airbnb Clone

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>© 2020 Airbnb Clone</p>
+            <p>Privacy
+            <span role="img" aria-labelledby="aria-label"> ◾ </span> Terms
+            <span role="img" aria-labelledby="aria-label"> ◾ </span>  Sitemap
+            <span role="img" aria-labelledby="aria-label"> ◾ </span> Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
